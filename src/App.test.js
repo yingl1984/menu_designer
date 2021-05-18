f73bcec6a62809cfad42e5943130cb8b3517f0a3
Ying Liu <yingl1984@gmail.com>
2021-05-18 09:36:11 -0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,16 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import { shallow } from 'enzyme'
 import DishDetails from '../src/components/DishDetails'
+import Welcome from '../src/pages/Welcome';
+import MenuList from '../src/pages/MenuList';
+import AddDish from '../src/pages/AddDish';
 
+it('renders APP component without crashing', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper).toMatchSnapshot()
+});
+
+//Components
 it('renders Footer component without crashing', () => {
   const wrapper = shallow(<Footer />)
   expect(wrapper).toMatchSnapshot()
@@ -13,11 +22,22 @@ it('renders Header component without crashing', () => {
   const wrapper = shallow(<Header />)
   expect(wrapper).toMatchSnapshot()
 });
-it('renders APP component without crashing', () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper).toMatchSnapshot()
-});
-it('renders NoteView component without crashing', () => {
+
+it('renders DishDetails component without crashing', () => {
   const wrapper = shallow(<DishDetails />)
+  expect(wrapper).toMatchSnapshot()
+})
+
+//Pages
+it('renders Welcome component without crashing', () => {
+  const wrapper = shallow(<Welcome />)
+  expect(wrapper).toMatchSnapshot()
+})
+it('renders AddDish component without crashing', () => {
+  const wrapper = shallow(<AddDish />)
+  expect(wrapper).toMatchSnapshot()
+})
+it('renders MenuList component without crashing', () => {
+  const wrapper = shallow(<MenuList />)
   expect(wrapper).toMatchSnapshot()
 })
